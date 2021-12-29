a2e36d4799cb5987879e0e13819ed1cad98a7da9
Timirbulatov <aidar126621@gmail.com>
2021-12-29 17:53:58 +0400

    база данных, ввод новых записей, просмотрт 2/3 записей

--- main.py
+++ main.py
@@ -3,25 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///libra.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Autor(db.Model):
-    __tablename__ = 'autor'
-    idAutor = db.Column(db.Integer, primary_key=True)
-    autorName = db.Column(db.String(100), nullable=True)
+class Author(db.Model):
+    __tablename__ = 'author'
+    idAuthor = db.Column(db.Integer, primary_key=True)
+    authorName = db.Column(db.String(100), nullable=True)
     country = db.Column(db.String(100), nullable=True)
+    books = db.relationship('Book', backref='Author', lazy='dynamic')
 
     def __repr__(self):
-        return '<Autor %r>' % self.idAutor
+        return '<Author %r>' % self.idAuthor
 
 
 class Book(db.Model):
     __tablename__ = 'book'
     idBook = db.Column(db.Integer, primary_key=True)
-    idAutor = db.Column(db.Integer, db.ForeignKey('autor.idAutor'), nullable=False)
+    idAuthor = db.Column(db.Integer, db.ForeignKey('author.idAuthor'), nullable=False)
     title = db.Column(db.String(100), nullable=True)
     publisher = db.Column(db.String(100), nullable=True)
+    readers = db.relationship('Reader', backref='Book', lazy='dynamic')
 
     def __repr__(self):
         return '<Book %r>' % self.idBook
@@ -43,9 +46,72 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/about')
-def about():
-    return "About page"
+@app.route('/authors-base')
+def authors_base():
+    authors = Author.query.order_by(Author.authorName).all()
+    return render_template("authors-base.html", authors=authors)
+
+
+@app.route('/books-base')
+def books_base():
+    books = Book.query.order_by(Book.title).all()
+    return render_template("books-base.html", books=books)
+
+
+@app.route('/add-author', methods=['POST', 'GET'])
+def add_author():
+    if request.method == 'POST':
+        authorName = request.form['authorName']
+        country = request.form['country']
+
+        author = Author(authorName=authorName, country=country)
+
+        try:
+            db.session.add(author)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При добавлении автора произошла ошибка"
+    else:
+        return render_template("add-author.html")
+
+
+@app.route('/add-book', methods=['POST', 'GET'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        publisher = request.form['publisher']
+        idAuthor = request.form['idAuthor']
+
+        book = Book(title=title, publisher=publisher, idAuthor=idAuthor)
+
+        try:
+            db.session.add(book)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При добавлении книги произошла ошибка"
+    else:
+        return render_template("add-book.html")
+
+
+@app.route('/add-reader', methods=['POST', 'GET'])
+def add_reader():
+    if request.method == 'POST':
+        readerName = request.form['readerName']
+        address = request.form['address']
+        idBook = request.form['idBook']
+
+        book = Book(readerName=readerName, address=address, idBook=idBook)
+
+        try:
+            db.session.add(book)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "При добавлении книги произошла ошибка"
+    else:
+        return render_template("add-reader.html")
 
 
 if __name__ == "__main__":
